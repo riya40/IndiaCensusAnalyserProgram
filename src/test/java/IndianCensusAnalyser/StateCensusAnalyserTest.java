ada9f036dc7dcebef42aa0e2bd0991f86f81ab68
riya40 <priyanunna20@gmail.com>
2022-01-19 21:52:51 +0530

    Addedd:Test case for record match

---
+++ src/test/java/IndianCensusAnalyser/StateCensusAnalyserTest.java
@@ -0,0 +1,26 @@
+package IndianCensusAnalyser;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Unit test for StateCensusAnalyser.
+ */
+public class StateCensusAnalyserTest
+{
+    StateCensusAnalyser censusAnalyser=new StateCensusAnalyser();
+    CSVStateCensus csvStateCensus=new CSVStateCensus();
+
+    @Test
+    public void shouldReturnCheckthenumberofrecordsmatch() throws Exception {
+        String result=censusAnalyser.loaddatauseiterator();
+        String result1=csvStateCensus.loaddata();
+        Assertions.assertEquals(result,result1);
+        
+    }
+
+}
